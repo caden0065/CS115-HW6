c45276102d0feb619a9e745e8d9aef1f4374ec3d
caden <155499795+caden0065@users.noreply.github.com>
2026-02-18 21:37:06 +0000

    learned input/output and comments in python

---
+++ calculator_thing.py
@@ -0,0 +1,6 @@
+numberValue1 = float(input("Enter a value: "))
+numberValue2 = float(input("Enter a second value: "))
+
+numberSum = numberValue1 + numberValue2
+
+print(f"The two values ({numberValue1} and {numberValue2}) have a sum of: {numberSum}")
---
+++ lecture_notes.py
@@ -0,0 +1,27 @@
+x = 3
+y = 6
+
+cat_name = "bobby"
+
+cat_name = 6.25
+
+print(x)
+print(cat_name)
+
+print("this is a string")
+
+print("x is", x, "y is", y, "cat name is", cat_name)
+
+# fstrings
+print(f'x is {x} and y is {y}')
+
+""""
+this is a multi line comment
+nothing in here is executed
+"""
+
+answer = input("Enter a number: ")
+print(f'You entered {answer}')
+
+answer = x + y
+answer += 1
